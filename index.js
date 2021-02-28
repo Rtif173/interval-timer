efca1efc522da56b4363ea17f9b9b9feb2182cbd
Rtif173 <71215188+Rtif173@users.noreply.github.com>
2021-02-28 10:11:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-function timer([time, color]){
+function timer([time, txt, color]){
 	mainDiv.style.backgroundColor = color;
 	
 	if(time){
 		timeLabel.innerHTML = time;
 		time = time - 1;
-		
 		timerId = setInterval(function(){
+			
 			if(time < 0){
 				startNewInterval();
 			} else {
+				if (time == 0){
+					longBeep();
+				} else if (time<=3){
+					shortBeep();
+				};
 				timeLabel.innerHTML = time;
 				time = time - 1;
 			}
 		}, 1000);
 		
 	} else {
-		timeLabel.innerHTML = "WORK";
+		timeLabel.innerHTML = txt;
 	}
 };
 
 function startNewInterval(){
 	clearInterval(timerId);
-	try {
-		timer(krug[i]);
-	} catch {
-		i = i - howMuchExInKrug;
-		timer(krug[i]);
+	if (i > howMuchExInKrug - 1){
+		i = 0;
 		k = k + 1;
-		krugLabel.innerHTML = k + "/" + numberOfKrug;
-	}
+		if(k > numberOfKrug){
+			k = 1;
+			p = p + 1;
+		};
+	};
+	if (i == 0 && p == numberOfPodhody && k == numberOfKrug){
+		krug[howMuchExInKrug - 1] = [null, "FINISH", "#4B0082"];
+	};
+	krugLabel.innerHTML = k + "/" + numberOfKrug;
+	podhodLabel.innerHTML = p + "/" + numberOfPodhody;
+	timer(krug[i]);
 	i = i + 1;
+	
 };
 
 function parseArg(){
@@ -37,33 +49,73 @@ function parseArg(){
 	if(v) {
 		numberOfKrug = v[0];
 		numberOfPodhody = v[1];
+		krug[1][0] = v[2];
 	};
+};
+
+function longBeep() {
+  var audio = new Audio(); 
+  audio.src = 'sounds/beep.wav'; 
+  audio.autoplay = true; 
+}
+
+function shortBeep() {
+  var audio = new Audio(); 
+  audio.src = 'sounds/short_beep.wav'; 
+  audio.autoplay = true; 
 }
 
 let timeLabel = document.getElementById("time-label");
 let mainDiv = document.getElementById("main");
 let krugLabel = document.getElementById("krugLabel");
-let krug = [[null, "red"], [50, "blue"]];
+let podhodLabel = document.getElementById("podhodLabel");
+let krug = [[null, "WORK", "red"], [50, null, "blue"]];
 let howMuchExInKrug = krug.length;
 let k = 1;
-let i = 1;
+let p = 1;
+let i = 0;
 let timerId;
 let numberOfKrug = 4;
 let numberOfPodhody = 1;
+let softLeftIsRestart = false;
+
+function resetTimer(){
+	clearInterval(timerId);
+	p = 1;
+	k = 1;
+	i = 0;
+	parseArg();
+	document.getElementById("softLeftLabel").innerHTML = "Options";
+	mainDiv.style.backgroundColor = "#448FFF";
+	timeLabel.innerHTML = "---";
+	krugLabel.innerHTML = "-/"+numberOfKrug;
+	podhodLabel.innerHTML = "-/"+numberOfPodhody;
+}
 
 parseArg();
+krugLabel.innerHTML = "-/"+numberOfKrug;
+podhodLabel.innerHTML = "-/"+numberOfPodhody;
 
 window.addEventListener('keydown', function(event) {
+	//event.preventDefault()
 	switch(event.key){
 		case "Enter":
-			timer(krug[0]);
+			if(!softLeftIsRestart){
+				softLeftIsRestart = true;
+				document.getElementById("softLeftLabel").innerHTML = "Reset";
+				startNewInterval();
+			};
 			break;
-		case "ArrowRight":
 		case "SoftRight":
 			startNewInterval();
 			break;
-		case "Delete":
-			window.open("options/options.html", "_self")
+		case "SoftLeft":
+			if(!softLeftIsRestart){
+				window.open("options/options.html?"+numberOfKrug+"&"+numberOfPodhody+"&"+krug[1][0], "_self")
+			} else {
+				resetTimer();
+				softLeftIsRestart = false;
+			};
 			break;
 	}
 });
--- options/options.js
+++ options/options.js
@@ -1,46 +1,80 @@
 let nrows = 3;
 let ncols = 1;
-let navigatableElements = [];
+//let navigatableElements = [];
 let numberOfKrugi = 4;
 let numberOfPodhody = 5;
+let restTime = 50;
 
-for(i=1; i<=nrows; i++){
+let krugiLabel = document.getElementById("ei15_17_13_6");
+let podhodyLabel = document.getElementById("ei15_21_13_6");
+let restTimeLabel = document.getElementById("ei24_21_13_6");
+//let chboxRBF = document.getElementById("ei15_87_15_85_24_42");
+
+/*for(i=1; i<=nrows; i++){
 	navigatableElements[i-1] = [];
 	for(j=1; j<=ncols; j++){
 		console.log(i+""+j+"item");
 		navigatableElements[i-1][j-1] = document.getElementById(i+""+j+"item");
 	};
-};
+};*/
 
-console.log(navigatableElements);
+let navigatableElements = 	[[document.getElementById("ei15_17_14_0")]
+						,[document.getElementById("ei15_21_14_0")]
+						,[document.getElementById("ei24_21_14_0")]
+						,[document.getElementById("ei15_87_15_99")]];
+						
+//console.log(navigatableElements);
+
 let selectedElement = [0, 0];
 
+function parseArg(){
+	let v = location.search.slice(1).split("&");
+	console.log(v);
+	if(v) {
+		numberOfKrugi = parseInt(v[0]);
+		numberOfPodhody = parseInt(v[1]);
+		restTime = parseInt(v[2]);
+	};
+	
+};
+
 function verticlalNav(direction){
-	navigatableElements[selectedElement[0]][selectedElement[1]].style.backgroundColor = "#fff";
+	navigatableElements[selectedElement[0]][selectedElement[1]].style.background = "#C4C4C4";
 	console.log([selectedElement[0], direction])
 	if ((selectedElement[0] == 0) && (direction == -1)){
-		selectedElement[0] = 2;
+		selectedElement[0] = nrows-1;
 	} else {
 		selectedElement[0] = (selectedElement[0] + direction)%nrows;
 	};
-	navigatableElements[selectedElement[0]][selectedElement[1]].style.backgroundColor = "#C4C4C4";
-}
+	navigatableElements[selectedElement[0]][selectedElement[1]].style.background = "#000";
+};
 
 function gorizontalSelector(direction){
 	switch(selectedElement.join("")){
 		case "00":
 			numberOfKrugi = numberOfKrugi + direction;
-			document.getElementById("numberOfKrugi").innerHTML = numberOfKrugi;
+			krugiLabel.innerHTML = numberOfKrugi;
 			break;
 		case "10":
 			numberOfPodhody = numberOfPodhody + direction;
-			document.getElementById("numberOfPodhody").innerHTML = numberOfPodhody;
+			podhodyLabel.innerHTML = numberOfPodhody;
+			break;
+		case "20":
+			restTime = restTime + direction;
+			restTimeLabel.innerHTML = restTime;
 			break;
 	};
-}
+};
 
-verticlalNav(0);
+function startWindow(){
+	parseArg();
+	verticlalNav(0);
+	krugiLabel.innerHTML = numberOfKrugi;
+	podhodyLabel.innerHTML = numberOfPodhody;
+	restTimeLabel.innerHTML = restTime;
+};
 
+startWindow();
 window.addEventListener("keydown", function(event){
 	switch (event.key){
 		case "ArrowUp":
@@ -55,7 +89,7 @@ window.addEventListener("keydown", function(event){
 		case "ArrowLeft":
 			gorizontalSelector(-1);
 			break;
-		case "Delete":
-			window.open("../index.html?"+numberOfKrugi+"&"+numberOfPodhody, "_self");
+		case "SoftLeft":
+			window.open("../index.html?"+numberOfKrugi+"&"+numberOfPodhody+"&"+restTime, "_self");
 	};
 })
